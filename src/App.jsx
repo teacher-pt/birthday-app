9ef64dd301a2ecfe8409e73072014f9ae453343a
teacher-pt <aditeacherpt@gmail.com>
2025-12-31 01:56:48 +0200

    feat: update App component to support dynamic text direction based on selected language

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,25 @@ import './App.css'
 import ChooseLanguage from './components/ChooseLanguage'
 import BirthDate from './components/BirthDate'
 import GreetingsList from './components/GreetingsList'
+import { LanguageContext } from './LanguageContext';
+import { useContext } from 'react';
 
 function App() {
+    const { language } = useContext(LanguageContext);
+
+    const dir = language === 'he' ? 'rtl' : 'ltr';
+
     return (
-        <>
+        <div dir={dir}>
             <ChooseLanguage />
             <BirthDate birthdate={new Date(2010, 0, 1)} />
             <GreetingsList greetings={[
                 "Happy Birthday!",
                 "Wishing you a wonderful year ahead.",
-                "May all your dreams come true."
+                "May all your dreams come true.",
+                "Have a fantastic celebration!"
             ]} />
-        </>
+        </div>
     )
 }
 
