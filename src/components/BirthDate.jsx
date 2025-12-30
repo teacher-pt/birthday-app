6079bebadf5a0c6fa0d8d804658090e42d68bf96
teacher-pt <aditeacherpt@gmail.com>
2025-12-31 01:45:49 +0200

    feat: enhance BirthDate component to dynamically translate age and birthdate label

--- src/components/BirthDate.jsx
+++ src/components/BirthDate.jsx
@@ -1,16 +1,29 @@
-import { useContext } from "react";
-import { LanguageContext } from "../LanguageContext";
+import { useContext, useEffect, useState } from 'react';
+import { LanguageContext } from '../LanguageContext';
+import { translate } from '../utils';
 
 export default function BirthDate({ birthdate = new Date() }) {
     const { language } = useContext(LanguageContext);
 
+    const [hText, setHText] = useState('');
+    const [pText, setPText] = useState('');
+
     const now = new Date();
     const age = now.getFullYear() - birthdate.getFullYear();
 
+    useEffect(() => {
+        translate(`Congratulations! You are ${age} years old`, 'en', language).then(setHText);
+    }, [language, age]);
+
+    useEffect(() => {
+        translate('Your date of birth:', 'en', language).then(setPText);
+    }, [language]);
+
+
     return (
         <div>
-            <h1>Congratulations! You are {age} years old</h1>
-            <p>Your date of birth: {birthdate.toLocaleDateString(language)}</p>
+            <h1>{hText}</h1>
+            <p>{pText} {birthdate.toLocaleDateString(language)}</p>
         </div>
     )
 }
--- src/components/GreetingCard.jsx
+++ src/components/GreetingCard.jsx
@@ -1,6 +1,8 @@
 import './GreetingCard.css';
+import { translate } from '../utils';
 
 export default function GreetingCard({ greeting }) {
+    
     return (
         <div className="greeting-card">
             {greeting}
