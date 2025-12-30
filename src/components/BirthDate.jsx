ffa20ae91cc0f3d06340ee4cca3e714cc7a5cfa6
teacher-pt <aditeacherpt@gmail.com>
2025-12-31 01:29:21 +0200

    feat: update BirthDate component to display birthdate in selected language format

--- src/components/BirthDate.jsx
+++ src/components/BirthDate.jsx
@@ -1,11 +1,16 @@
+import { useContext } from "react";
+import { LanguageContext } from "../LanguageContext";
+
 export default function BirthDate({ birthdate = new Date() }) {
+    const { language } = useContext(LanguageContext);
+
     const now = new Date();
     const age = now.getFullYear() - birthdate.getFullYear();
 
     return (
         <div>
             <h1>Congratulations! You are {age} years old</h1>
-            <p>Your date of birth: {birthdate.toDateString()}</p>
+            <p>Your date of birth: {birthdate.toLocaleDateString(language)}</p>
         </div>
     )
 }
