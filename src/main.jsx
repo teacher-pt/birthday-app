7dff487e9b9b8416cc0f664d5d00347e81731cdc
teacher-pt <aditeacherpt@gmail.com>
2025-12-31 01:12:03 +0200

    feat: wrap App component with LanguageContextProvider for language management

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import LanguageContextProvider from './LanguageContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <LanguageContextProvider>
+            <App />
+        </LanguageContextProvider>
+    </StrictMode>,
 )
