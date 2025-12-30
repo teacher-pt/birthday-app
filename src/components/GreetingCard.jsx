8533e37dd115c65df36e853a73024bf4fa5029cf
teacher-pt <aditeacherpt@gmail.com>
2025-12-31 01:49:05 +0200

    feat: update GreetingCard component to translate and display greetings based on selected language

--- src/components/GreetingCard.jsx
+++ src/components/GreetingCard.jsx
@@ -1,11 +1,19 @@
 import './GreetingCard.css';
 import { translate } from '../utils';
+import { useContext, useEffect, useState } from 'react';
+import { LanguageContext } from '../LanguageContext';
 
 export default function GreetingCard({ greeting }) {
-    
+    const { language } = useContext(LanguageContext);
+    const [translatedGreeting, setTranslatedGreeting] = useState(greeting);
+
+    useEffect(() => {
+        translate(greeting, 'en', language).then(setTranslatedGreeting);
+    }, [greeting, language]);
+
     return (
         <div className="greeting-card">
-            {greeting}
+            {translatedGreeting}
         </div>
     )
 }
