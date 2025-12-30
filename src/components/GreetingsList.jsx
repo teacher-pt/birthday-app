838b0303ba74757cb9f60075a7e792f31620fdb4
teacher-pt <aditeacherpt@gmail.com>
2025-12-31 01:54:36 +0200

    feat: enhance GreetingsList component to display translated header based on selected language

--- src/components/GreetingsList.jsx
+++ src/components/GreetingsList.jsx
@@ -1,11 +1,25 @@
+import { useContext, useEffect, useState } from "react";
 import GreetingCard from "./GreetingCard";
+import { LanguageContext } from "../LanguageContext";
+import { translate } from "../utils";
 
 export default function GreetingsList({ greetings = [] }) {
+    const { language } = useContext(LanguageContext);
+
+    const [hText, setHText] = useState('Greeting List');
+
+    useEffect(() => {
+        translate('Greeting List', 'en', language).then(setHText);
+    }, [language]);
+
     return (
         <div>
-            {greetings.map((greeting, index) => (
-                <GreetingCard key={index} greeting={greeting} />
-            ))}
+            <h2>{hText}</h2>
+            <div>
+                {greetings.map((greeting, index) => (
+                    <GreetingCard key={index} greeting={greeting} />
+                ))}
+            </div>
         </div>
     )
 }
