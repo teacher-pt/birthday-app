f8b34d16661386bb13cb019db5fcf7e568ca9f9b
teacher-pt <aditeacherpt@gmail.com>
2026-01-07 11:45:12 +0200

    feat: add support for translating arrays of words in the translate utility function

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,9 @@
 export const translate = async (word, source = 'en', target) => {
+    if (Array.isArray(word)) {
+        return Promise.all(word.map(w => translate(w, source, target)));
+    }
+
+
     const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(word)}&langpair=${source}|${target}`;
     try {
         const response = await fetch(url);
